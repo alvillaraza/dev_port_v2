f03cb7967a2510f32f00c858739592a114d7f48e
alexis villaraza <alexis.borja@gmail.com>
2021-02-02 11:26:08 -0700

    added tech stack icons

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,17 +2,39 @@ import React from "react"
 
 import github from "../assets/logo-github.png"
 import linkedin from "../assets/logo-linked-in.png"
+import html from "../assets/logo-html-5.png"
+import css from "../assets/logo-css.png"
+import sass from "../assets/logo-sass.png"
+import less from "../assets/logo-less.png"
+import bootstrap from "../assets/logo-bootstrap.png"
+import js from "../assets/logo-js.png"
+import react from "../assets/logo-react.png"
+import redux from "../assets/logo-redux.png"
+import python from "../assets/logo-python.png"
+import git from "../assets/logo-git.png"
 
 export default function LandingPage() {
   return (
     <>
       <div className="links">
-        <img src={github}></img>
-        <img src={linkedin}></img>
+        <img src={github} alt='github icon' />
+        <img src={linkedin} alt='linked in icon' />
       </div>
       <div className="name-title">
         <h1>Alexis Villaraza</h1>
         <h2>FULL-STACK WEB DEVELOPER</h2>
+      </div>
+          <div className="tech-stack-icons">
+              <img src={html} alt="html icon" />
+              <img src={css} alt="css icon" />
+              <img src={sass} alt="sass icon" />
+              <img src={less} alt="less icon" />
+              <img src={bootstrap} alt="bootstrap icon" />
+              <img src={js} alt="js icon" />
+              <img src={react} alt="react icon" />
+              <img src={redux} alt="redux icon" />
+              <img src={python} alt="python icon" />
+              <img src={git} alt="git icon" />
       </div>
     </>
   )
