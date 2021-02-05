f457ee8ae0b125ced353e0d477495b51c278d397
alexis villaraza <alexis.borja@gmail.com>
2021-02-05 11:32:41 -0700

    attempt favicon update

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,18 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'React go',
+        short_name: 'Reactgo',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'standalone',
+        icon: 'src/assets/favicon.png',
+      },
+    }
+  ],
 }
