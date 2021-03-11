0c5d7b875d64cc538cb422facee36030d267c890
Alexis Villaraza <alvillaraza@Alexiss-MacBook-Air.local>
2021-03-11 11:09:52 -0700

    fixed icons

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -26,7 +26,7 @@ export default function Portfolio({ open, setOpen, page }) {
             rel="noopener noreferrer"
           >
             <h1 className="first-line">
-              <img src={link} /> ACNH Villager Picker
+              <img src={link} alt="link icon" /> ACNH Villager Picker
             </h1>
           </a>
 
@@ -38,21 +38,24 @@ export default function Portfolio({ open, setOpen, page }) {
             track of their chosen or rejected animals.
             <ul>
               <li>
-                <img src={lightning} />
+                <img src={lightning} alt="lightning icon" />
                 Used an ACNH API to display character information
               </li>
               <li>
-                <img src={lightning} />
+                <img src={lightning} alt="lightning icon" />
                 Created a reusable interactive quiz complete with a scoreboard
                 at the end
               </li>
               <li>
-                <img src={lightning} />
+                <img src={lightning} alt="lightning icon" />
                 Implemented advanced design using LESS
               </li>
             </ul>
             Github:
-            <a href="https://github.com/alvillaraza/animal-crossing" target="_blank">
+            <a
+              href="https://github.com/alvillaraza/animal-crossing"
+              target="_blank"
+            >
               https://github.com/alvillaraza/animal-crossing
             </a>
           </p>
@@ -66,7 +69,7 @@ export default function Portfolio({ open, setOpen, page }) {
           >
             <h1 className="first-line">
               {" "}
-              <img src={link} /> Wellness Tracker
+              <img src={link} alt="link icon" /> Wellness Tracker
             </h1>
           </a>
           <h2 className="second-line">
@@ -77,45 +80,53 @@ export default function Portfolio({ open, setOpen, page }) {
             overall wellness.
             <ul>
               <li>
-                <img src={lightning} />
+                <img src={lightning} alt="lightning icon" />
                 An employee can sign in and track their sleep, water intake,
                 exercise, and breaks.
               </li>
               <li>
-                <img src={lightning} />A company admin can also sign in to see
-                each of the employee’s scores within each of their departments.
+                <img src={lightning} alt="lightning icon" />A company admin can
+                also sign in to see each of the employee’s scores within each of
+                their departments.
               </li>
               <li>
-                <img src={lightning} />
+                <img src={lightning} alt="lightning icon" />
                 Contributed to a cross-functional team of 4 developers and 1 UX
                 designer on an 8 week sprint with 3 canvas releases.
               </li>
               <li>
-                <img src={lightning} /> Applied JSON web token to develop user
-                credential authentication.
+                <img src={lightning} alt="lightning icon" /> Applied JSON web
+                token to develop user credential authentication.
               </li>
               <li>
-                <img src={lightning} /> Developed API calls and endpoints.
+                <img src={lightning} alt="lightning icon" /> Developed API calls
+                and endpoints.
               </li>
               <li>
-                <img src={lightning} />
+                <img src={lightning} alt="lightning icon" />
                 Implemented features and debugged code on the frontend and
                 backend.
               </li>
               <li>
-                <img src={lightning} /> Organized pair programming sessions to
-                increase efficiency.
+                <img src={lightning} alt="lightning icon" /> Organized pair
+                programming sessions to increase efficiency.
               </li>
             </ul>
             <p>
               Github Frontend:{" "}
-              <a href="https://github.com/alvillaraza/refresh-fe" target="_blank">
+              <a
+                href="https://github.com/alvillaraza/refresh-fe"
+                target="_blank"
+              >
                 https://github.com/Lambda-School-Labs/refresh-fe
               </a>
             </p>
             <p>
               Github Backend:{" "}
-              <a href="https://github.com/Lambda-School-Labs/refresh-be" target="_blank">
+              <a
+                href="https://github.com/Lambda-School-Labs/refresh-be"
+                target="_blank"
+              >
                 https://github.com/Lambda-School-Labs/refresh-be
               </a>
             </p>
@@ -129,22 +140,22 @@ export default function Portfolio({ open, setOpen, page }) {
             rel="noopener noreferrer"
           >
             <h1 className="first-line">
-              <img src={link} /> Opti
+              <img src={link} alt="link icon" /> Opti
             </h1>
           </a>
           <h2 className="second-line">HTML, LESS</h2>
           <p className="description">
+            A website designed to find the best optimal price for an airbnb
+            rental property.
             <ul>
-              A website designed to find the best optimal price for an airbnb
-              rental property.
               <li>
-                <img src={lightning} />
+                <img src={lightning} alt="lightning icon" />
                 Participated as a UI developer in a cross-functional team of 3
                 developers and 4 data scientists on a weeklong project.
               </li>
               <li>
-                <img src={lightning} /> Designed and implemented the landing
-                page using LESS.
+                <img src={lightning} alt="lightning icon" /> Designed and
+                implemented the landing page using LESS.
               </li>
             </ul>
             Github:{" "}
